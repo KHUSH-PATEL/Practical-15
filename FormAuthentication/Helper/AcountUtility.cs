480eba450b4002ca75c528bf10d3878f25c5af90
Khush <khush@simformsolutions.com>
2023-06-20 19:17:38 +0530

    All test have been included

---
+++ FormAuthentication/Helper/AcountUtility.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace BankingSystem.Helper
+{
+    public static class AcountUtility
+    {
+        public static string GenerateAccountNumber()
+        {
+            Random random = new Random();
+            int length = random.Next(9, 17); // Generate a random length between 9 and 16
+            StringBuilder accountNumber = new StringBuilder();
+
+            for (int i = 0; i < length; i++)
+            {
+                accountNumber.Append(random.Next(0, 10)); // Generate a random digit between 0 and 9
+            }
+
+            return accountNumber.ToString();
+        }
+        public static string GenerateIFSCCode()
+        {
+            Random random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // Define the characters allowed in IFSC code
+
+            StringBuilder ifscCode = new StringBuilder();
+            for (int i = 0; i < 4; i++)
+            {
+                ifscCode.Append(chars[random.Next(chars.Length)]); // Generate a random character from the allowed characters
+            }
+            ifscCode.Append("0"); // Append a zero as the fifth character
+            for (int i = 0; i < 6; i++)
+            {
+                ifscCode.Append(random.Next(0, 10)); // Generate a random digit between 0 and 9
+            }
+
+            return ifscCode.ToString();
+        }
+    }
+}
---
+++ FormAuthentication/Models/SignUp.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FormAuthentication.Models
+{
+    public class SignUp
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required!")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email is required!")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; } = string.Empty;
+
+
+        [NotMapped]
+        [Required(ErrorMessage = "Password is required!")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,15}$",
+            ErrorMessage = "Password must be between 6 and 20 characters and contain one uppercase letter, one lowercase letter, one digit and one special character.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        [NotMapped]
+        [Required(ErrorMessage = "Confirm Password is required!")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password and compare password should be same!")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+
+        public string salt { get; set; } = string.Empty;
+        public string hashedPass { get; set; } = string.Empty;
+    }
+}
